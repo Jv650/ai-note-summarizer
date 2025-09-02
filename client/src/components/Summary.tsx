096595d41a10d4f1ec71b1dbfc7988ae720a0a73
Jv650 <jemmyvictor1@gmail.com>
2025-09-01 20:39:15 -0700

    fix: temp comment out search input

--- client/src/components/Summary.tsx
+++ client/src/components/Summary.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import '../css/App.css';
+import isImage from "./App";
 
 
 const ReturnedSummary = () => {
 return (
+    <div>
+    {/* {isImage && (
     <div className="render-picture">
-        <img></img>
+    <img src={isImage}/>
     </div>
+)} */}
+</div>
 );
 }
 export default ReturnedSummary;
